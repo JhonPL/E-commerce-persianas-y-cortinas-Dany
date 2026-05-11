36167ecfb86602557ce1febf2b031795cc180149
Samuel Pacheco <samuelentrenandolatinos@gmail.com>
2026-05-11 15:03:25 -0500

    feat: update ALLOWED_HOSTS and add security settings in production configuration

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -2,11 +2,26 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
-CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS")
-CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["ucc.bluelineroofingconstruction.com"])
+CORS_ALLOWED_ORIGINS = env.list(
+	"CORS_ALLOWED_ORIGINS",
+	default=["https://ucc.bluelineroofingconstruction.com"],
+)
+CSRF_TRUSTED_ORIGINS = env.list(
+	"CSRF_TRUSTED_ORIGINS",
+	default=["https://ucc.bluelineroofingconstruction.com"],
+)
+GOOGLE_OAUTH_ALLOWED_REDIRECT_URIS = env.list(
+	"GOOGLE_OAUTH_ALLOWED_REDIRECT_URIS",
+	default=["https://ucc.bluelineroofingconstruction.com/oauth/google"],
+)
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = env.bool("SECURE_SSL_REDIRECT", default=True)
+
+SECURE_HSTS_SECONDS = env.int("SECURE_HSTS_SECONDS", default=31536000)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True)
+SECURE_HSTS_PRELOAD = env.bool("SECURE_HSTS_PRELOAD", default=True)
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
